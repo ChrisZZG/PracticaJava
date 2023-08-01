8cffb7e5e70e44d001f43f5784654c8400e978e0
ChrisZZG <cristiangonzz@gmail.com>
2023-08-01 20:27:05 -0300

    Hasta examenes de Matrizes y Vectores

--- src/main/java/com/mycompany/holamundo2/HolaMundo2.java
+++ src/main/java/com/mycompany/holamundo2/HolaMundo2.java
@@ -16,7 +16,7 @@ public class HolaMundo2 {
     public static void main(String[] args) {
        
         //Realizar un contador de numeros y el tope maximo lo establecera el usuario
-        System.out.println("ESTRUCTURAS REPETITIVAS");
+        System.out.println("-----ESTRUCTURAS REPETITIVAS-------");
 //        System.out.println("------CONTADOR------------");
 //        System.out.println("Ingresar el Valor que sera el Tope Maximo");
 //        Scanner teclado = new Scanner(System.in);
@@ -95,43 +95,209 @@ public class HolaMundo2 {
         
         
         System.out.println("---VECTORES Y MATRICES------------");
-        System.out.println("IMPLEMENTACION");
-        int vector[] = new int [4];
-        vector[0] = 25;
-        vector[1] = 30;
-        vector[2] = 35;
-        vector[3] = 40;
-        System.out.println("---Recorrido del Vector-----");
-        System.out.println("My vector de Edades: ");
-        for(int i=0; i< vector.length; i++){
-            System.out.println("Estoy en la posicion: " + i + ", y su valor es: " + vector[i]);
+//        System.out.println("IMPLEMENTACION");
+//        int vector[] = new int [4];
+//        vector[0] = 25;
+//        vector[1] = 30;
+//        vector[2] = 35;
+//        vector[3] = 40;
+//        System.out.println("---Recorrido del Vector-----");
+//        System.out.println("My vector de Edades: ");
+//        for(int i=0; i< vector.length; i++){
+//            System.out.println("Estoy en la posicion: " + i + ", y su valor es: " + vector[i]);
+//        }
+//        
+//        System.out.println("CARGA DE VALORES POR SCANNER");
+//        int edades[] = new int [5];
+//        
+//        Scanner valores = new Scanner(System.in);
+//        
+//        for(int i=0; i<edades.length; i++){
+//            System.out.println("Ingresar el valor para la posicion: " + i);
+//            edades[i] = valores.nextInt();
+//            
+//        }
+//        System.out.println("Mostramos como quedo el Vector por Scanner");
+//        for(int i=0; i<edades.length; i++){
+//            System.out.println("Para la posicion: " + i + ", el valor es: " + edades[i]);
+//            
+//        }
+        
+        //System.out.println("----Ejemplo 2 - VECTORES----");
+        
+//        String nombres[] = new String [4];
+//        
+//        System.out.println("Ingresar el nombre correspondiente para cada posicion: ");
+//        Scanner nombre = new Scanner(System.in);
+//        for(int i = 0; i<nombres.length; i++){
+//            System.out.println("Ingresar el valor para la posicion: " + i);
+//            nombres[i] = nombre.next();
+//            
+//        }
+//        
+//        System.out.println("Valores del Vector NOMBRES: ");
+//        for(int i= 0; i<nombres.length; i++){
+//            System.out.println("Para la posicion: " + i + "el valor es: " + nombres[i]);
+//        }
+
+//         System.out.println("----Ejemplo 3 - VECTORES---");
+//         
+//         String apellidos[] = new String[4];
+//         Scanner ingresoApe = new Scanner(System.in);
+//         for(int i=0; i<apellidos.length; i++){
+//             System.out.println("Ingresar el apellido para la posicion: " + i);
+//             apellidos[i] = ingresoApe.next();
+//         }
+//         for(int i = 0; i<apellidos.length; i++){
+//             System.out.println("Para la posicion: " + i + " : el apellido es: " + apellidos[i]);
+//         }
+         
+        
+        //System.out.println("-----MATRICES--------");
+        
+//        int matriz[][] = new int [2][2];
+//        
+//        System.out.println("Ingresar los valore para cada posicion");
+//        Scanner valor = new Scanner(System.in);
+//        
+//        for(int f=0; f<2; f++){
+//            for(int c=0; c<2; c++){
+//                System.out.println("Ingresar valor para: " + f + " y " + c);
+//                matriz[f][c] = valor.nextInt();
+//            }
+//        }
+//        
+//        System.out.println("Mostrar la matriz");
+//         for(int f=0; f<2; f++){
+//             for(int c=0; c<2; c++){
+//                 System.out.println("El valor para f: " + f + " y c: " + c + ", es de: " + matriz[f][c]);
+//             }
+//         }
+        
+        //System.out.println("--------Ejemplo 2 - MATRICES");
+        
+//        String amigos[][] = new String [2][2];
+//        Scanner entradaAmi = new Scanner(System.in);
+//        
+//        for(int f = 0; f<amigos.length; f++){
+//            for(int c = 0; c<amigos.length; c++){
+//                System.out.println("Ingresar el Apellido para la posicion: " + f + c);
+//                amigos[f][c] = entradaAmi.next();
+//            }
+//        }
+//        for(int f=0; f<amigos.length; f++){
+//            for(int c=0; c<amigos.length; c++){
+//                System.out.println("Para la posicion: " + f + " y " + c + " , el Nombre es: " + amigos[f][c]);
+//            }
+//        }
+        
+          System.out.println("--Examen 1 --- VECTORES Y MATRICES");
+          
+//          System.out.println("Vector de 15 valores e Identificacion del numero 3");
+//          
+//          int quince[] = new int[15];
+//          Scanner valorInd = new Scanner(System.in);
+//          
+//          for(int i= 0; i<quince.length; i++){
+//              System.out.println("Ingresar el Numero para la posicion: " + i);
+//              quince[i] = valorInd.nextInt();
+//          }
+//          
+//          int recuento = 0;
+//          for(int i=0; i<quince.length; i++){
+//              int provisorio = quince[i];
+//              if(provisorio == 3){
+//                  recuento = recuento + 1 ;
+//                  
+//             }
+//              
+//          }
+//        System.out.println("El numero 3, aparece: " + recuento + " veces.");
+        
+        
+        System.out.println("--Examen 2 --- VECTORES Y MATRICES");
+        
+//        Double notas[][] = new Double [4][4];
+//        Scanner notasInd = new Scanner(System.in);
+//        Double sumaPromedio = 0.0;
+//        
+//        System.out.println("---------Carga de Notas------");
+//        for(int f=0; f<notas.length; f++){
+//            System.out.println("Ingresar las Notas del Alumno: " + (f+1));
+//            for(int c=0; c<notas.length - 1; c++){
+//                System.out.println("Nota: " + c + " :");
+//                notas[f][c] = notasInd.nextDouble();
+//                sumaPromedio = sumaPromedio + notas[f][c];
+//                
+//            }
+//            notas[f][3] = sumaPromedio/3;
+//            sumaPromedio = 0.0;
+//        }
+//        
+//        System.out.println("Mostrar las Notas y Promedio de cada alumno");
+//        
+//        int semestre = 1;
+//        
+//        for(int f=0; f<notas.length; f++){
+//            System.out.println("Las notas del alumno: " + (f+1));
+//            for(int c=0; c<notas.length - 1; c++){
+//                
+//                System.out.println("semestre: " + semestre + ": " + notas[f][c]);
+//                semestre = semestre +1;
+//                
+//            }
+//            System.out.println("Su Promedio es de: " + notas[f][3]);
+//            semestre = 1;
+//        }
+        
+        System.out.println("--Examen 3 --- VECTORES Y MATRICES");
+//        
+//        String hermanos[] = new String[8];
+//        Scanner ingresoNombres = new Scanner(System.in);
+//        
+//        for(int i=0; i<hermanos.length; i++){
+//            System.out.println("Ingresar el Nombre para la fila: " + i);
+//            hermanos[i] = ingresoNombres.next();
+//        }
+//        
+//        for(int i=0; i<hermanos.length; i++){
+//            System.out.println("Para la  posicion: " + i + ": " + hermanos[i]);
+//        }
+        
+        System.out.println("--Examen 4 --- VECTORES Y MATRICES");
+        
+        String ciudad[] = new String[5];
+        int minima[] = new int[5];
+        int maxima[] = new int[5];
+        
+        System.out.println("Carga Ciudades");
+        Scanner cargaCiudad = new Scanner(System.in);
+        for(int c = 0; c<ciudad.length; c++){
+            System.out.println("Cargar la ciudad para la posicion: " + c);
+            ciudad[c] = cargaCiudad.next();
         }
         
-        System.out.println("CARGA DE VALORES POR SCANNER");
-        int edades[] = new int [5];
-        
-        Scanner valores = new Scanner(System.in);
-        
-        for(int i=0; i<edades.length; i++){
-            System.out.println("Ingresar el valor para la posicion: " + i);
-            edades[i] = valores.nextInt();
-            
-        }
-        System.out.println("Mostramos como quedo el Vector por Scanner");
-        for(int i=0; i<edades.length; i++){
-            System.out.println("Para la posicion: " + i + ", el valor es: " + edades[i]);
-            
+        System.out.println("Carga Temperaturas Minima");
+        Scanner cargaMinima = new Scanner(System.in);
+        for(int mi = 0; mi<minima.length; mi++){
+            System.out.println("Cargar la Temperatura minima para la posicion: " + mi);
+            minima[mi] = cargaMinima.nextInt();
         }
         
         
+        System.out.println("Carga Temperaturas Maximas");
+        Scanner cargaMaxima = new Scanner(System.in);
+        for(int ma=0; ma<maxima.length; ma++){
+            maxima[ma] = cargaMaxima.nextInt();
+        }
         
-        
-            
-        
-            
-            
-        
-        
+        System.out.println("Mostrar la Ciudad con la Temperatura mas Baja y mas Alta");
+        int baja = 0;
+        for(int c=0; c<ciudad.length; c++){
+            for(int mi=0; mi<minima.length; mi++){
+                baja = minima[mi];
+            }
+        }
         
     }
    
